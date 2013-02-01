3fb15fc6072d08ded966cbc3d255f5f638a83be6
Man Fong <mlfong@gatech.edu>
2013-02-01 18:45:32 -0500

    start team

---
+++ CHA/Map1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Map1ViewController.h
+//  CHA
+//
+//  Created by mfong on 2/1/13.
+//  Copyright (c) 2013 Brian Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Map1ViewController : UIViewController
+
+@end
---
+++ CHA/Map1ViewController.m
@@ -0,0 +1,38 @@
+//
+//  Map1ViewController.m
+//  CHA
+//
+//  Created by mfong on 2/1/13.
+//  Copyright (c) 2013 Brian Sun. All rights reserved.
+//
+
+#import "Map1ViewController.h"
+
+@interface Map1ViewController ()
+
+@end
+
+@implementation Map1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
