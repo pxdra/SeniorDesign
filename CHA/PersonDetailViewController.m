8a40012e53f2a9035c03b1fd0aaab5c161463a72
Brian Sun <pxdra.y@gmail.com>
2013-03-18 16:03:45 -0700

    Person detailed view

---
+++ CHA/PersonDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  PersonDetailViewController.m
+//  CHA
+//
+//  Created by Brian Sun on 3/18/13.
+//  Copyright (c) 2013 Brian Sun. All rights reserved.
+//
+
+#import "PersonDetailViewController.h"
+
+@interface PersonDetailViewController ()
+
+@end
+
+@implementation PersonDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
